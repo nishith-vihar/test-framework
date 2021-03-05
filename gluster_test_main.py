6619fb235b37986d057b3934b0e5dc6092e9d647
Nishith Vihar Sakinala <nsakinal@redhat.com>
2021-03-05 17:17:34 +0530

    Added basic config parameters parsing apis
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>

--- gluster_test_main.py
+++ gluster_test_main.py
@@ -1,9 +1,23 @@
-from gluster_test_parser import generate_config_hashmap
+from gluster_test_parser import (generate_config_hashmap,get_server_ip_list,
+                                get_client_ip_list,get_server_ip,get_client_ip,
+                                get_brick_root_list,volume_create_force_option)
 
 def main():
     """Function to call the config file parse library"""
     config_hashmap = generate_config_hashmap()
-    print(config_hashmap)
+    #print(config_hashmap)
+    server_ip_list = get_server_ip_list(config_hashmap)
+    client_ip_list = get_client_ip_list(config_hashmap)
+    server_ip = get_server_ip("server_vm1",config_hashmap)
+    client_ip = get_client_ip("client_vm1",config_hashmap)
+    brick_root_list = get_brick_root_list("client_vm1",config_hashmap)
+    volume_create_force = volume_create_force_option(config_hashmap)
+    #print(server_ip_list)
+    #print(client_ip_list)
+    #print(server_ip)
+    #print(client_ip)
+    #print(brick_root)
+    #print(volume_create_force)
 
 
 if __name__ == '__main__':
--- gluster_test_parser.py
+++ gluster_test_parser.py
@@ -10,7 +10,7 @@ def load_yaml(filename):
     """Function to read config file"""
     configfd = open_file(filename)
     if configfd:
-        config = yaml.load(configfd)
+        config = yaml.load(configfd, Loader=yaml.FullLoader)
         return config
     return None
 
@@ -22,7 +22,6 @@ def handle_config(config_file):
         return ret
 
     return False
-
 
 def parse_args():
     """Parse arguments with newer argparse module
@@ -37,7 +36,6 @@ def parse_args():
                         default=None)
     return parser.parse_args()
 
-
 def generate_config_hashmap():
     """Function to generate hashmap"""
 
@@ -46,4 +44,94 @@ def generate_config_hashmap():
     if args.config_file:
         config_hashmap = handle_config(args.config_file)
         return config_hashmap
+
+def get_server_ip(server_name,config_hashmap):
+    """ Gives the server ip given the server name
+    Args:
+        server_name: name of the server as in config file.
+                     example: server_vm1
+        config_hashmap: the config file dictionary generated
+                        while parsing.
+    Returns:
+        ip address of the given server
+    Example:
+        get_server_ip("server_vm1",config_hashmap)
+    """
+    index = server_name[9:]
+    server_ip = config_hashmap['servers'][int(index)-1]
+    return server_ip
+
+def get_client_ip(client_name,config_hashmap):
+    """ Gives the client ip given the client name
+    Args:
+        client_name: name of the client as in config file.
+                     example: client_vm1
+        config_hashmap: the config file dictionary generated
+                        while parsing.
+    Returns:
+        ip address of the given client
+    Example:
+        get_client_ip("client_vm1",config_hashmap)
+    """
+    index = client_name[9:]
+    client_ip = config_hashmap['clients'][int(index)-1]
+    return client_ip
+
+def get_server_ip_list(config_hashmap):
+    """ Gives the list of all server ip
+    Args:
+        config_hashmap: the config file dictionary generated
+                        while parsing.
+    Returns:
+        list of all server ip address
+    Example:
+        get_server_ip_list(config_hashmap)
+    """
+    server_ip_list = config_hashmap['servers']
+    return server_ip_list
+
+def get_client_ip_list(config_hashmap):
+    """ Gives the list of all client ip
+    Args:
+        config_hashmap: the config file dictionary generated
+                        while parsing.
+    Returns:
+        list of all client ip address
+    Example:
+        get_client_ip_list(config_hashmap)
+    """
+    client_ip_list = config_hashmap['clients']
+    return client_ip_list
+
+def get_brick_root_list(server_name,config_hashmap):
+    """ Returns the list of brick root given the server name
+    Args:
+        server_name: name of the server as in config file.
+                     example: server_vm1
+        config_hashmap: the config file dictionary generated
+                        while parsing.
+    Returns:
+        ilist of brick root of the given server
+    Example:
+        get_brick_root_list("server_vm1",config_hashmap)
+    """
+    server_ip = get_server_ip(server_name,config_hashmap)
+    brick_root_list = config_hashmap['servers_info'][server_ip]['brick_root']
+    return brick_root_list
+
+def volume_create_force_option(config_hashmap):
+    """ Returns the flag for volume_create_force option
+    Args:
+        config_hashmap: the config file dictionary generated
+                        while parsing.
+    Returns:
+        flag value(True/False) for volume_create_force option
+    Example:
+        volume_create_force_option(config_hashmap)
+    """
+    volume_create_force = config_hashmap['gluster']["volume_create_force"]
+    return volume_create_force
+
+
+
 
