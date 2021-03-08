d9b29397bf5739ba111daadf1a6bbd8a69a7a893
Nishith Vihar Sakinala <nsakinal@redhat.com>
2021-03-08 18:34:26 +0530

    Redesign of parsing module The changes made avoids the input of config_hashmap for parsing config.
    Signed-off-by: Nishith Vihar Sakinala <nsakinal@redhat.com>

--- gluster_test_main.py
+++ gluster_test_main.py
@@ -1,24 +1,19 @@
-from gluster_test_parser import (generate_config_hashmap,get_server_ip_list,
-                                get_client_ip_list,get_server_ip,get_client_ip,
-                                get_brick_root_list,volume_create_force_option)
+from gluster_test_parser import Parser
 
 def main():
-    """Function to call the config file parse library"""
-    config_hashmap = generate_config_hashmap()
-    #print(config_hashmap)
-    server_ip_list = get_server_ip_list(config_hashmap)
-    client_ip_list = get_client_ip_list(config_hashmap)
-    server_ip = get_server_ip("server_vm1",config_hashmap)
-    client_ip = get_client_ip("client_vm1",config_hashmap)
-    brick_root_list = get_brick_root_list("client_vm1",config_hashmap)
-    volume_create_force = volume_create_force_option(config_hashmap)
-    #print(server_ip_list)
-    #print(client_ip_list)
-    #print(server_ip)
-    #print(client_ip)
-    #print(brick_root)
-    #print(volume_create_force)
-
+    parser = Parser()
+    server_ip_list = parser.get_server_ip_list()
+    client_ip_list = parser.get_client_ip_list()
+    server_ip = parser.get_server_ip("server_vm1")
+    client_ip = parser.get_client_ip("client_vm1")
+    brick_root_list = parser.get_brick_root_list("client_vm1")
+    volume_create_force = parser.volume_create_force_option()
+    print(server_ip_list)
+    print(client_ip_list)
+    print(server_ip)
+    print(client_ip)
+    print(brick_root_list)
+    print(volume_create_force)
 
 if __name__ == '__main__':
     main()
